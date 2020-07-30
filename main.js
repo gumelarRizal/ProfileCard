eaf378ae055656a37355b80a909587b2f9dd30c1
RizalGumelar <mrizalgumelar@gmail.com>
2020-07-30 13:44:12 +0700

    first Commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const heading = 'AMEL BUTUT';
+let i = 0;
+
+const typing = () => {
+    if (i < heading.length) {
+        document.querySelector('.heading').innerHTML += heading.charAt(i);
+        i++;
+        setTimeout(typing, 60);
+    }
+};
+
+typing();
